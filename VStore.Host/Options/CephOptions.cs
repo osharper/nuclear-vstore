3c630e9b038e0d22fc17880f8a4a0a88a7acd1aa
Denis Ivanov <denis@ivanovdenis.ru>
2016-09-21 19:01:49 +0700

    CephOptions updated

--- VStore.Host/Options/CephOptions.cs
+++ VStore.Host/Options/CephOptions.cs
@@ -1,12 +1,7 @@
-﻿using System;
-
-namespace NuClear.VStore.Host.Options
+﻿namespace NuClear.VStore.Host.Options
 {
     public sealed class CephOptions
     {
-        public Uri EndpointUrl { get; set; }
         public string BucketName { get; set; }
-        public string AccessKey { get; set; }
-        public string SecretKey { get; set; }
     }
 }
