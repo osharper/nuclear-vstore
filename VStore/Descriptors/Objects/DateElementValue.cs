fdcff7cd9a504a8e7a005fbbf72734ccd186888b
Nikita Balabaev <n.balabaev@2Gis.ru>
2016-12-15 17:39:04 +0700

    CR fixes - remove Raw from DateElementValue

--- VStore/Descriptors/Objects/DateElementValue.cs
+++ VStore/Descriptors/Objects/DateElementValue.cs
@@ -4,8 +4,6 @@ namespace NuClear.VStore.Descriptors.Objects
 {
     public class DateElementValue : IObjectElementValue
     {
-        public string Raw { get; set; }
-
         public DateTime BeginDate { get; set; }
 
         public DateTime EndDate { get; set; }
--- VStore/Objects/ContentValidation/LinkValidator.cs
+++ VStore/Objects/ContentValidation/LinkValidator.cs
@@ -11,14 +11,15 @@ namespace NuClear.VStore.Objects.ContentValidation
     {
         public static IEnumerable<ObjectElementValidationError> CheckLink(IObjectElementValue value, IElementConstraints constraints)
         {
-            if (string.IsNullOrEmpty(value.Raw))
+            var rawValue = (IObjectElementRawValue)value;
+            if (string.IsNullOrEmpty(rawValue.Raw))
             {
                 return Array.Empty<ObjectElementValidationError>();
             }
 
             // We can use Uri.IsWellFormedUriString() instead:
             Uri uri;
-            if (!Uri.TryCreate(value.Raw, UriKind.Absolute, out uri)
+            if (!Uri.TryCreate(rawValue.Raw, UriKind.Absolute, out uri)
                 || (uri.Scheme != "http" && uri.Scheme != "https")
                 || uri.HostNameType != UriHostNameType.Dns)
             {
