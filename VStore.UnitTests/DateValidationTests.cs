b3cb1611518b93c0f9acf3e91bf4b76ae4dfe364
Nikita Balabaev <n.balabaev@2Gis.ru>
2016-12-16 10:49:10 +0700

    add step in GitLab CI to run unit tests

---
+++ VStore.UnitTests/DateValidationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using NuClear.VStore.Descriptors.Objects;
+using NuClear.VStore.Objects.ContentValidation;
+using NuClear.VStore.Objects.ContentValidation.Errors;
+
+using Xunit;
+
+namespace VStore.UnitTests
+{
+    public class DateValidationTests
+    {
+        [Fact]
+        public void TestDateRangeValidation()
+        {
+            var value = new DateElementValue { BeginDate = DateTime.Today, EndDate = DateTime.Today.AddDays(1) };
+
+            var error = TestHelpers.MakeCheck<DateElementValue, InvalidDateRangeError>(
+                value,
+                null,
+                DateValidator.CheckDate,
+                val => val.EndDate = DateTime.Today.AddMinutes(-1));
+            Assert.StrictEqual(ElementValidationErrors.InvalidDateRange, error.ErrorType);
+        }
+    }
+}
