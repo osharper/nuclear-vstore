5d779a8504ee3b111908b1e65484895c4a35b64d
Denis Ivanov <denis@ivanovdenis.ru>
2017-01-11 18:25:56 +0700

    running unit test while app building

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 
 dotnet restore
+dotnet test VStore.UnitTests -c release -r ubuntu.14.04-x64
 rm -rf $(pwd)/publish/vstore
 dotnet publish VStore.Host/project.json -c release -r ubuntu.14.04-x64 -o $(pwd)/publish/vstore
