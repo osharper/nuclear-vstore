5ae2ca5e577e40973ad7c1b9de3531ff156cf2f8
Denis Ivanov <denis@ivanovdenis.ru>
2017-06-09 19:43:30 +0700

    basic event sending and reading

---
+++ src/VStore/Kafka/EventSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+using NuClear.VStore.Options;
+
+namespace NuClear.VStore.Kafka
+{
+    public sealed class EventSender
+    {
+        private readonly Dictionary<string, object> _producerConfig = new Dictionary<string, object>();
+        public EventSender(KafkaOptions kafkaOptions)
+        {
+            _producerConfig.Add("bootstrap.servers", kafkaOptions.BrokerEndpoints);
+        }
+
+        public async Task Send(string topic, string message)
+        {
+            using (var producer = new Producer<Null, string>(_producerConfig, null, new StringSerializer(Encoding.UTF8)))
+            {
+                await producer.ProduceAsync(topic, null, message);
+            }
+        }
+    }
+}
